efdbe2699033b1aac0e64a32103ed79caabf8f76
AmyLingJs <lingjs90@163.com>
2019-01-19 16:14:26 +0800

    提交demo1.vue

---
+++ demo1.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in list" 
+               :key="index" 
+               @click="highlight(index)"
+               :class="{active:current===index &&current !== ' '}">
+               {{item}}
+              
+            </li>
+             <button type="button" @click="add">add</button>
+        </ul>
+        <ul>
+            <li
+              v-for="(item,index) in targets" 
+              :key="index"
+            >{{item}}
+              <button type="button" @click="delete1(index)">delete</button>
+            </li>
+        </ul>
+        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            list:[1,2,3,4,5,6,7,8,9],
+            targets:[],
+            current:''
+        }
+    },
+    methods: {
+        highlight:function (index) {
+            this.current = index
+        },
+        add:function(index) {
+            if(this.current === "") { return}
+            this.targets.push(this.list[this.current])
+            this.current = ''
+
+        },
+        delete1:function(index) {
+            this.targets.splice(index,1)
+        }
+    }
+}
+</script>
+<style scoped>
+li.active {
+    background-color: green;
+}
+</style>
+
+
--- problems.js
+++ problems.js
@@ -3,3 +3,22 @@
 * 1.v-on:click无效，无法执行事件，f12查看发现是button在el元素也就是id为
                     app的div外部                  
  */
+
+
+ /** 
+  * 升级npm：npm install -g npm@latest
+  * vue create my-vue1或者vue ui可视化创建项目
+  * 
+  * vue组件化的思想：独立 可复用 整体化
+  * 为什么要组件化：复用 高效 开发复杂的单页面应用
+  * 组件拆分的原则：300行原则 复用原则 业务复杂性原则
+  * 
+  * 阅读vue风格指南https://cn.vuejs.org/v2/style-guide/
+  * 
+  * 
+  * 直接运行新创建的demo.vue文件
+  * vue serve demo.vue
+  * 注意：需要全局安装vue serve:npm install -g @vue/cli-service-global
+  * 安装时间有点点久，耐心等待
+  * 安装成功后,执行，浏览器打开http://localhost:8080/
+ */
