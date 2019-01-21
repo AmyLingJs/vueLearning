d1589fe4495ed177002a042c1d6fef213e8bda91
AmyLingJs <lingjs90@163.com>
2019-01-22 06:27:09 +0800

    aaa

--- demo2/src/views/Add.vue
+++ demo2/src/views/Add.vue
@@ -1,25 +1,41 @@
 <template>
     <div>
         <span>标题</span> 
-        <div> <input type="text" /></div>
+        <div> <input type="text" v-model="title" /></div>
        
         内容
         <div>
-             <input type="textarea" />
+             <input type="textarea" v-model="content"/>
         </div>
        <button type="button" @click="addNews">完成</button>
        <button type="button" @click="cancelInput">取消</button>
     </div>
 </template>
 <script>
+import store from '../store.js'
 export default {
     name:'Add',
+    store,
+    data() {
+        return {
+            title:'',
+            content:''
+
+        }
+    },
     methods:{
         addNews() {
-            console.log(11111111111111)
+            store.commit('addItem',{
+                title:this.title,
+                content:this.content
+            })
+            this.title="";
+            this.content="";
+            
         },
         cancelInput() {
-            console.log('cancel')
+            this.title="";
+            this.content="";
         }
     }
 }
--- demo2/src/views/List.vue
+++ demo2/src/views/List.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        新闻列表
-        <router-view></router-view>
+      <ul>
+          <li v-for="(item,index) in pageLists" 
+           :key="index">
+          {{item.title}}-{{item.content}}
+          </li>
+      </ul>
     </div>
 </template>
 <script>
+import store from '@/store'
 export default {
-    name:'List'
+    name:'List',
+    store,
+    computed:{
+        pageLists() {
+            return store.state.lists
+        }
+    }
 }
 </script>
 
